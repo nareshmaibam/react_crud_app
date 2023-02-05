85b9468fb3e0b896d4504fe340c5a7a540a709d6
nareshmaibam <nareshhmaibam@gmail.com>
2023-02-05 13:52:58 +0530

    CRUD app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Home from "./component/Home";
+import Add from "./component/Add";
+import Update from "./component/Update";
+import View from "./component/View";
+import Delete from "./component/Delete";
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <div>
+          <ul>
+            <li>
+              <Link to={"/add"}>Add</Link>
+            </li>
+            <li>
+              <Link to={"/view"}>View</Link>
+            </li>
+          </ul>
+        </div>
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/view" element={<View />} />
+          <Route path="/add" element={<Add />} />
+          <Route path="/update" element={<Update />} />
+          <Route path="/delete" element={<Delete />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
